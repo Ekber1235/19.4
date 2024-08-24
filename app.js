7be839c8a5d883c43dedfe35e464794041c80170
Ekber1235 <ekber160309@gmail.com>
2024-08-24 18:32:21 +0400

    home

---
+++ app.js
@@ -0,0 +1,10 @@
+let fullName = "Əkbər Mərdanzadə"
+let result  = fullName.split('')
+console.log(result.length);
+
+
+    return str.includes('e');
+
+let sampleString = "example";
+console.log(containsE(sampleString)); // true
+
